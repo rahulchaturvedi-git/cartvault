f893eaf1a806a25f5e55ef6fe975e0c5d3453a2e
rahulchaturvedi-git <rahul.chaturvedi23@spit.ac.in>
2026-02-10 21:59:05 +0530

    Phase 3: Added React dashboard with item fetch and delete

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import products_collection
 from datetime import datetime
 from bson import ObjectId
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Home route
